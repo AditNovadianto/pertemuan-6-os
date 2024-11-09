8bcd8d5e8e6a8bbc01ecb7d7ffdf8e8eb563966d
Aditya <aditya.novadianto@gmail.com>
2024-11-09 14:22:42 +0700

    first commit

---
+++ pertemuan-6/percobaan_array.sh
@@ -0,0 +1,12 @@
+NAMES=""
+NAMES="${NAMES} Aditya Novadianto Pratama"
+NAMES="${NAMES} Alfi"
+NAMES="${NAMES} Thariq"
+
+echo ’One at a time...’
+for NAME in ${NAMES}; do
+echo ${NAME};
+done
+
+echo ’All together now!’
+echo ${NAMES}
