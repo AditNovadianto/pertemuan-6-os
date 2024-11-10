0217c2603e2b843f50e0419f6e0977fd6d9f9b76
AditNovadianto <aditya.novadianto@gmail.com>
2024-11-11 01:06:46 +0700

    update ' in all file

--- pertemuan-6/percobaan_array.sh
+++ pertemuan-6/percobaan_array.sh
@@ -3,10 +3,10 @@ NAMES="${NAMES} Aditya Novadianto Pratama"
 NAMES="${NAMES} Alfi"
 NAMES="${NAMES} Thariq"
 
-echo ’One at a time...’
+echo 'One at a time...'
 for NAME in ${NAMES}; do
 echo ${NAME};
 done
 
-echo ’All together now!’
+echo 'All together now!'
 echo ${NAMES}
--- pertemuan-6/percobaan_array_kedua.sh
+++ pertemuan-6/percobaan_array_kedua.sh
@@ -8,31 +8,31 @@ FRUITS="${FRUITS} KIWI"
 FRUITS="${FRUITS} APEL"
 FRUITS="${FRUITS} STRAWBERRY"
 
-echo ’Inilah daftar buah-buahan...’
+echo 'Inilah daftar buah-buahan...'
 for BUAH in ${FRUITS}; do
 echo ${BUAH};
 done
 
-echo ’Sekarang daftar semua buah adalah : ’
+echo 'Sekarang daftar semua buah adalah : '
 echo ${FRUITS}
 
-echo ’Nama buah kedua adalah : ’
-echo ${FRUITS} | cut -d’ ’ -f2
+echo 'Nama buah kedua adalah : '
+echo ${FRUITS} | cut -d' ' -f2
 
-echo ’Nama buah keempat adalah’
-echo ${FRUITS} | cut -d’ ’ -f4
+echo 'Nama buah keempat adalah'
+echo ${FRUITS} | cut -d' ' -f4
 
-echo ’Tambahkan 1 nama buah...’
+echo 'Tambahkan 1 nama buah...'
 FRUITS="${FRUITS} DURIAN"
 
-echo ’Nama buah kesembilan adalah’
-echo ${FRUITS} | cut -d’ ’ -f9
+echo 'Nama buah kesembilan adalah'
+echo ${FRUITS} | cut -d' ' -f9
 
-echo ’Nama buah kesepuluh adalah’
-echo ${FRUITS} | cut -d’ ’ -f10
+echo 'Nama buah kesepuluh adalah'
+echo ${FRUITS} | cut -d' ' -f10
 
-echo ’Tambahkan 1 lagi nama buah...’
+echo 'Tambahkan 1 lagi nama buah...'
 FRUITS="${FRUITS} DUKU"
 
 echo ’Nama buah kesepuluh adalah’
-echo ${FRUITS} | cut -d’ ’ -f10
+echo ${FRUITS} | cut -d' ' -f10
