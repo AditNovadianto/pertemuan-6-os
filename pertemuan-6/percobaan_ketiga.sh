3973fd2abd8cd779ea2f863a53724d4f463900ee
Aditya <aditya.novadianto@gmail.com>
2024-11-11 16:14:48 +0700

    add file percobaan_ketiga

---
+++ pertemuan-6/percobaan_ketiga.sh
@@ -0,0 +1,7 @@
+echo ``Sebutkan tiga nama panggilan teman-teman Anda xxxx yyyy zzzz !``
+read panggilan1 panggilan2 panggilan3
+echo ``Nama panggilan teman pertama Anda adalah : $panggilan1``
+echo ``Nama panggilan teman kedua Anda adalah : $panggilan2``
+echo ``Nama panggilan teman ketiga Anda adalah : $panggilan3``
+set $panggilan1 $panggilan2 $panggilan3
+echo ``Anda memiliki teman sebanyak : $#``
